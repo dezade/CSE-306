451a5431a20ce59c6effc501013f524ffc40eb9d
dezade <zmsiddiqee2002@gmail.com>
2024-02-12 20:02:37 +0600

    MIPS HARDWARE EVALUATION DONE, only viva left, repo reorg

--- MIPS/A1_Group3_Submission/A1_Group3_Necessary_Contents/Assembler/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bison -d -y parser.y
-g++ -w -c -o y.o y.tab.c
-flex lexer.lex
-g++ -w -c -o l.o lex.yy.c
-g++ y.o l.o -lfl -o code.out
-./code.out instructionsAssembly.txt CLIJMHOEBKFGDNAP
